6af31ebc3f1a9c58090dcdc7989b5ead728a99e4
Ali Sufyan <malikali051002@gmail.com>
2025-02-10 23:21:05 +0500

    updated

--- server/routes/otpRoutes.js
+++ server/routes/otpRoutes.js
@@ -5,12 +5,12 @@ const { sendOtpController, verifyOtpController,sendRegOtpController,verifyRegOtp
 const router = express.Router();
 
 // Route to send email
-router.post('/send-otp',authorize(['user','government official','InspectionOfficer']), sendOtpController);
+router.post('/send-otp', sendOtpController);
 
-router.post('/verify-otp',authorize(['user','government official','InspectionOfficer']), verifyOtpController);
+router.post('/verify-otp', verifyOtpController);
 
-router.post('/send-Regotp',authorize(['user','government official','InspectionOfficer']), sendRegOtpController);
+router.post('/send-Regotp', sendRegOtpController);
 
-router.post('/verify-Regotp',authorize(['user','government official','InspectionOfficer']), verifyRegOtpController);
+router.post('/verify-Regotp', verifyRegOtpController);
 
 module.exports = router;
